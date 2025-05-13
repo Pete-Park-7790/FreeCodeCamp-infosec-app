4cf3473782d14fee0c592656e63600fc6c56325e
Harshit Singhal <harshitsinghal7790@gmail.com>
2025-05-13 20:01:39 +0530

    Task-9

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+
+
+
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet.hsts({
   maxAge: ninetyDaysInSeconds,
